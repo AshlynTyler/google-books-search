ad1e7596012f1e7210c49effca7197ff5426750b
Cyber Witch <no-reply>
2019-07-01 20:34:08 -0600

    added all api routes

--- server.js
+++ server.js
@@ -28,6 +28,31 @@ app.get("/api/books", function(req,res){
   });
 })
 
+app.post("/api/books", function(req,res){
+  Books.create(req.body)
+  .then(function(dbUser) {
+    // If saved successfully, send the the new User document to the client
+    res.json(dbUser);
+  })
+  .catch(function(err) {
+    // If an error occurs, send the error to the client
+    res.json(err);
+  });
+})
+
+app.delete("/api/books/:id", function(req,res){
+  Books.findByIdAndDelete(req.params, function(error,book){
+    if(error)
+      res.send({
+        status: 301,
+      message: `could not delete ${req.params.id}`})
+    else
+      res.send({
+        status: 200,
+      message: `deleted ${req.params.id} successfully!`})
+  })
+})
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
